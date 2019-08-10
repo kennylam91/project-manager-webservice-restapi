eb0f8e0e0e44e15493b76f2bf8ae8434db774a80
kennylam91 <phamvanlam1991@gmail.com>
2019-08-10 10:45:23 +0700

    first commit, run ok

---
+++ src/main/java/com/codegym/pms/model/Project.java
@@ -0,0 +1,41 @@
+package com.codegym.pms.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
